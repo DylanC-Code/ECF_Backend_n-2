8191faabcb35a46388b960f22e0d6bc8f22ad00c
Dylan <dylan.castor06@gmail.com>
2022-08-05 16:15:59 +0200

    Design ended

--- src/Views/Exam/edit.php
+++ src/Views/Exam/edit.php
@@ -6,20 +6,20 @@
 
   <form action="/exams/edit/<?= $data['id'] ?>" method="POST" class="w-2/5 h-[50vh] mx-auto bg-white px-8 rounded-lg">
     <h1 class="title py-8">Modifier ou supprimer l'élève</h1>
-    <fieldset class="flex my-8 p-4">
+    <fieldset class="flex my-8 p-4 items-center">
       <label class="w-[200px] px-8 block">Changer la matiere</label>
-      <select name="matiere" value="<?= $data['matiere'] ?>" class="block">Changer matière
+      <select name="matiere" value="<?= $data['matiere'] ?>" class="input rounded-lg">Changer matière
         <option value="Histoire-Geographie" <?php if ($data['matiere'] == 'Histoire-Geographie') echo 'selected' ?>>Histoire-Geographie</option>
         <option value="Mathématiques" <?php if ($data['matiere'] == 'Mathématiques') echo 'selected' ?>>Mathématiques</option>
       </select>
     </fieldset>
 
-    <fieldset class="flex my-8 p-4">
+    <fieldset class="flex my-8 p-4 items-center">
       <label class="block w-[200px] px-8">Mofidier la note</label>
-      <input type="number" name="note" step="0.01" value="<?= $data['note'] ?>" class="block">
+      <input type="number" name="note" step="0.01" value="<?= $data['note'] ?>" class="input rounded-lg">
     </fieldset>
 
-    <fieldset class="w-full flex justify-around">
+    <fieldset class="w-full flex justify-around items-center">
       <button type="submit" class="button">Modifier</button>
       <a href="/exams/delete/<?= $data['id'] ?>" class="button">Supprimer</a>
     </fieldset>
--- src/Views/Student/edit.php
+++ src/Views/Student/edit.php
@@ -1,23 +1,22 @@
-<section>
-
-  <div class="mx-auto my-8 flex w-2/4 justify-between items-center">
-    <h1 class="text-2xl">Eleve n°<?= $data['id_etudiant'] ?></h1>
-    <h2 class="text-3xl"><?= $data['prenom'] ?> <?= $data['nom'] ?></h2>
+<section class="p-8">
+  <div class="mx-auto flex w-2/4 justify-between items-center">
+    <h1 class="text-2xl pb-8">Eleve n°<?= $data['id_etudiant'] ?></h1>
+    <h2 class="text-3xl pb-8"><?= $data['prenom'] ?> <?= $data['nom'] ?></h2>
   </div>
 
   <form action=""></form>
-  <form action="/students/edit/<?= $data['id_etudiant'] ?>" method="post" class="bg-white w1/3 mx-auto">
-    <h1 class="title">Modifier ou supprimer un élève</h1>
-    <fieldset>
-      <legend>Changer prenom</legend>
-      <input name="prenom" type="text" value="<?= $data['prenom'] ?>">
+  <form action="/students/edit/<?= $data['id_etudiant'] ?>" method="post" class="w-2/5 h-[50vh] mx-auto bg-white px-8 rounded-lg">
+    <h1 class="title py-8">Modifier ou supprimer un élève</h1>
+    <fieldset class="flex my-8 p-4 items-center">
+      <label class="w-[200px] px-8 block">Changer prenom</label>
+      <input name="prenom" type="text" value="<?= $data['prenom'] ?>" class="input">
     </fieldset>
-    <fieldset>
-      <legend>Changer nom</legend>
-      <input name="nom" type="text" value="<?= $data['nom'] ?>">
+    <fieldset class="flex my-8 p-4 items-center">
+      <label class="w-[200px] px-8 block">Changer nom</label>
+      <input name="nom" type="text" value="<?= $data['nom'] ?>" class="input">
     </fieldset>
 
-    <fieldset>
+    <fieldset class="w-full flex justify-around items-center">
       <button type="submit" class="button">Editer</button>
       <a href="/students/delete/<?= $data['id_etudiant'] ?>" class="button">Supprimer</a>
     </fieldset>
